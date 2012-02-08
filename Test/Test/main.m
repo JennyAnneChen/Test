092ddc3e7c077b558aa8e053e26df7fcd6e28872
Anne Chen <jenny.chen@microtek.com.tw>
2012-02-08 11:52:27 +0800

    a

---
+++ Test/Test/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Test
+//
+//  Created by jenny chen on 12/2/8.
+//  Copyright 2012年 microtek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
