f9357ecf462d74875029da406804a058c2e0bfec
Anne Chen <jenny.chen@microtek.com.tw>
2012-02-08 11:54:28 +0800

    Test

--- Test/Test/main.m
+++ Test/Test/main.m
@@ -14,7 +14,7 @@ int main (int argc, const char * argv[])
     @autoreleasepool {
         
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"Hello, World! Just Test");
         
     }
     return 0;
